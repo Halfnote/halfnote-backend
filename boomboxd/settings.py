683c26ae76a26fd26d3d57af2a7cf6effa19510a
Vivek Datta <viv360@Viveks-MBP.MG8702>
2025-04-16 22:13:13 -0700

    Fix JWT AUTH_COOKIE setting

--- boomboxd/settings.py
+++ boomboxd/settings.py
@@ -89,6 +89,12 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
+    'AUTH_COOKIE': 'access_token',
+    'AUTH_COOKIE_REFRESH': 'refresh_token',
+    'AUTH_COOKIE_SECURE': True,
+    'AUTH_COOKIE_HTTP_ONLY': True,
+    'AUTH_COOKIE_SAMESITE': 'Lax',
+    'AUTH_COOKIE_PATH': '/',
 }
 
 # REST Framework
