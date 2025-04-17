6a9453168e9c0ef6b503905b9228624a58510fcd
Vivek Datta <viv360@Viveks-MBP.MG8702>
2025-04-16 21:27:13 -0700

    Fix build script execution for Vercel

--- build_files.sh
+++ build_files.sh
@@ -2,13 +2,18 @@
 # Build script for Vercel deployment
 
 echo "Building the project..."
-python -m pip install -r requirements.txt
+pip install -r requirements.txt
 
-echo "Running migrations..."
+echo "Make DB migrations..."
 python manage.py makemigrations
+python manage.py makemigrations music
+python manage.py makemigrations reviews
+python manage.py makemigrations accounts
+
+echo "Apply DB migrations..."
 python manage.py migrate
 
 echo "Collecting static files..."
-python manage.py collectstatic --no-input
+python manage.py collectstatic --noinput
 
 echo "Build completed successfully!" 
